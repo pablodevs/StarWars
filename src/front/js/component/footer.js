4a8abe7ac24dd9be4c6a774e7f918915b7243a62
peibol888 <pabloalamovargas@gmail.com>
2022-02-04 15:36:02 +0000

    finished

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export const Footer = () => (
 	<footer className="footer mt-auto py-5 text-center sticky-bottom">
-		<p>&#169; Pablo & Jaime. All rigths reserved</p>
+		<p>&#169; Pablo - All rigths reserved</p>
 	</footer>
 );
